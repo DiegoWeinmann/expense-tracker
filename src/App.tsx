f6e2bfce7ad86b70e7924d6c346a3f479538b79b
Diego Weinmann <diego.weinmann@accenture.com>
2020-04-21 11:35:05 -0300

    feat: Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <h1>React app</h1>
+      <Header />
     </div>
   );
 }
